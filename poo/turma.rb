bb77c1e99ca0ebe50a6b5d936e44d03b7ac8cbce
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-11-11 16:42:52 -0300

    ADD private to atribute alunos in class Turma

--- poo/turma.rb
+++ poo/turma.rb
@@ -1,10 +1,25 @@
 class Turma
 
+  attr_reader :alunos
   attr_accessor :nome_turma, :numero_sala,:professor_responsavel
+
 
   def initialize(nome_turma,numero_sala,professor_responsavel)
     @nome_turma = nome_turma
     @numero_sala = numero_sala
     @professor_responsavel = professor_responsavel
+    @alunos = []
   end
+
+  def adiciona_aluno(aluno)
+    alunos << aluno
+  end
+
+  def total_alunos
+    puts(alunos.length())
+  end
+
+
+  private attr_writer :alunos
+
 end
