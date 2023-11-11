e300764a65db585249b7aa7a12ef52faafe875a7
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-11-11 15:19:52 -0300

    ADD methods inicio_ferias and fim_ferias to class professor

--- poo/professor.rb
+++ poo/professor.rb
@@ -1,10 +1,26 @@
 class Professor
 
-  attr_accessor  :nome, :codigo_funcional
+  attr_reader :ferias, :inicio_ferias,:fim_ferias
+  attr_accessor  :nome, :codigo_funcional, :disciplina
 
-  def initialize(nome,codigo_funcional)
+  def initialize(nome,codigo_funcional,disciplina)
     @nome = nome
     @codigo_funcional = codigo_funcional
+    @disciplina = disciplina
+    @ferias = false
+    @inicia_ferias
+    @fim_ferias
 
   end
+
+  def inicio_ferias()
+    @ferias = true
+    @inicio_ferias = Time.now()
+  end
+
+  def fim_ferias()
+    @ferias = false
+    @fim_ferias = Time.now()
+  end
+
 end
